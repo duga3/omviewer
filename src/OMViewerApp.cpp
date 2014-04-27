c0c8a2f06cdbd939fe54d0bec4987294be7ccd2b
Miroslav Kobetski <miroslav.kobetski@gmail.com>
2014-04-28 01:32:43 +0200

    - Formatting - bug overwriting actions - depth images no longer truncate

--- src/OMViewerApp.cpp
+++ src/OMViewerApp.cpp
@@ -7,9 +7,12 @@ using namespace cimg_library;
 namespace omviewer {
 void OMViewerApp::run()
 {
-	CImg<float> im = im_list_.getImage( 0 );
-	CImgDisplay main_disp( im );
+	cur_im_ = im_list_.getImage( 0 );
+	
+	CImgDisplay main_disp( cur_im_ );
 	main_disp.set_title( im_list_.getCurrentFileName().c_str() );
+	equalize_depth_image_ = cur_im_.max() > 255 ? true : false;
+	histogram_equalize_ = false;
 	display( main_disp );
 }
 
@@ -53,7 +56,6 @@ int OMViewerApp::getSelection( const cimg_library::CImg<float>& subim, cimg_libr
 																			upper_left_[2],
 																			reset_3d_view,
 																			has_depth_channels );
-
 	if( disp.wheel() )
 	{
 		if( disp.is_keyCTRLLEFT() || disp.is_keyCTRLRIGHT() )
@@ -112,7 +114,7 @@ int OMViewerApp::getSelection( const cimg_library::CImg<float>& subim, cimg_libr
 
 		if( sx0 == sx1 && sy0 == sy1 && sz0 == sz1 )
 		{
-			reset_view_ = true;
+			view_action_ = ViewAction::reset_view;
 		}
 		resize_disp_ = true;
 	}
@@ -121,8 +123,10 @@ int OMViewerApp::getSelection( const cimg_library::CImg<float>& subim, cimg_libr
 		switch( key = disp.key() )
 		{
 			case cimg::keyHOME:
-			case cimg::keyESC: reset_view_ = resize_disp_ = true;
-				key						   = 0;
+			case cimg::keyESC:
+				view_action_ = ViewAction::reset_view;
+				resize_disp_ = true;
+				key			 = 0;
 				break;
 
 			case cimg::keyPADADD: view_action_ = ViewAction::zoom_in;
@@ -143,221 +147,236 @@ int OMViewerApp::getSelection( const cimg_library::CImg<float>& subim, cimg_libr
 			case cimg::keyPAD6: view_action_ = ViewAction::goto_previous_frame;
 				key							 = 0;
 				break;
-		} // switch
+			case cimg::keyD:
+				equalize_depth_image_ = !equalize_depth_image_;
+				break;
+			case cimg::keyH:
+				histogram_equalize_ = !histogram_equalize_;
+				break;
+		}         // switch
+	}
+	return key;
+} // OMViewerApp::getSelection
 
-		if( view_action_ == ViewAction::zoom_in )
+void OMViewerApp::performViewAction(cimg_library::CImgDisplay & disp)
+{
+	if( view_action_ == ViewAction::zoom_in )
+	{
+		const int mx = center_view_ ? disp.width() / 2 : disp.mouse_x();
+		const int my = center_view_ ? disp.height() / 2 : disp.mouse_y();
+		const int mX = mx * cur_im_.width()  + ( cur_im_.depth() > 1 ? cur_im_.depth() : 0 ) / disp.width();
+		const int mY = my * cur_im_.height() + ( cur_im_.depth() > 1 ? cur_im_.depth() : 0 ) / disp.height();
+		int X		 = XYZ_[0];
+		int Y		 = XYZ_[1];
+		int Z		 = XYZ_[2];
+
+		if( mX < cur_im_.width() && mY < cur_im_.height() )
 		{
-			const int mx = center_view_ ? disp.width() / 2 : disp.mouse_x();
-			const int my = center_view_ ? disp.height() / 2 : disp.mouse_y();
-			const int mX = mx * cur_im_.width()  + ( cur_im_.depth() > 1 ? cur_im_.depth() : 0 ) / disp.width();
-			const int mY = my * cur_im_.height() + ( cur_im_.depth() > 1 ? cur_im_.depth() : 0 ) / disp.height();
-			int X		 = XYZ_[0];
-			int Y		 = XYZ_[1];
-			int Z		 = XYZ_[2];
-
-			if( mX < cur_im_.width() && mY < cur_im_.height() )
-			{
-				X = upper_left_[0] + mX * ( lower_right_[0] - upper_left_[0] + 1 ) / cur_im_.width();
-				Y = upper_left_[1] + mY * ( lower_right_[1] - upper_left_[1] + 1 ) / cur_im_.height();
-				Z = XYZ_[2];
-			}
-
-			if( mX < cur_im_.width() && mY >= cur_im_.height() )
-			{
-				X = upper_left_[0] + mX * ( lower_right_[0] - upper_left_[0] + 1 ) / cur_im_.width();
-				Z = upper_left_[2] + mY * ( lower_right_[2] - upper_left_[2] + 1 ) / cur_im_.height();
-				Y = XYZ_[1];
-			}
-
-			if( mX >= cur_im_.width() && mY < cur_im_.height() )
-			{
-				X = XYZ_[0];
-				Y = upper_left_[1] + mY * ( lower_right_[1] - upper_left_[1] + 1 ) / cur_im_.height();
-				Z = upper_left_[2] + mY * ( lower_right_[2] - upper_left_[2] + 1 ) / cur_im_.height();
-			}
-
-			if( lower_right_[0] - upper_left_[0] > 4 )
-			{
-				upper_left_[0]	= X - 7 * ( X - upper_left_[0] ) / 8;
-				lower_right_[0] = X + 7 * ( lower_right_[0] - X ) / 8;
-			}
-
-			if( lower_right_[1] - upper_left_[1] > 4 )
-			{
-				upper_left_[1]	= X - 7 * ( X - upper_left_[1] ) / 8;
-				lower_right_[1] = X + 7 * ( lower_right_[1] - X ) / 8;
-			}
-
-			if( lower_right_[2] - upper_left_[2] > 4 )
-			{
-				upper_left_[2]	= X - 7 * ( X - upper_left_[2] ) / 8;
-				lower_right_[2] = X + 7 * ( lower_right_[2] - X ) / 8;
-			}
+			X = upper_left_[0] + mX * ( lower_right_[0] - upper_left_[0] + 1 ) / cur_im_.width();
+			Y = upper_left_[1] + mY * ( lower_right_[1] - upper_left_[1] + 1 ) / cur_im_.height();
+			Z = XYZ_[2];
 		}
 
-		if( view_action_ == ViewAction::zoom_out )
+		if( mX < cur_im_.width() && mY >= cur_im_.height() )
 		{
-			const int delta_x  = ( lower_right_[0] - upper_left_[0] ) / 8;
-			const int delta_y  = ( lower_right_[1] - upper_left_[1] ) / 8;
-			const int delta_z  = ( lower_right_[2] - upper_left_[2] ) / 8;
-			const int ndelta_x = delta_x ? delta_x : ( cur_im_.width()  > 1 ? 1 : 0 );
-			const int ndelta_y = delta_y ? delta_y : ( cur_im_.height() > 1 ? 1 : 0 );
-			const int ndelta_z = delta_z ? delta_z : ( cur_im_.depth()  > 1 ? 1 : 0 );
-			upper_left_[0]	-= ndelta_x;
-			upper_left_[1]	-= ndelta_y;
-			upper_left_[2]	-= ndelta_z;
-			lower_right_[0] += ndelta_x;
-			lower_right_[1] += ndelta_y;
-			lower_right_[2] += ndelta_z;
+			X = upper_left_[0] + mX * ( lower_right_[0] - upper_left_[0] + 1 ) / cur_im_.width();
+			Z = upper_left_[2] + mY * ( lower_right_[2] - upper_left_[2] + 1 ) / cur_im_.height();
+			Y = XYZ_[1];
+		}
 
-			if( upper_left_[0] < 0 )
-			{
-				lower_right_[0] -= upper_left_[0];
-				upper_left_[0]	 = 0;
+		if( mX >= cur_im_.width() && mY < cur_im_.height() )
+		{
+			X = XYZ_[0];
+			Y = upper_left_[1] + mY * ( lower_right_[1] - upper_left_[1] + 1 ) / cur_im_.height();
+			Z = upper_left_[2] + mY * ( lower_right_[2] - upper_left_[2] + 1 ) / cur_im_.height();
+		}
 
-				if( lower_right_[0] >= cur_im_.width() )
-				{
-					lower_right_[0] = cur_im_.width() - 1;
-				}
-			}
+		if( lower_right_[0] - upper_left_[0] > 4 )
+		{
+			upper_left_[0]	= X - 7 * ( X - upper_left_[0] ) / 8;
+			lower_right_[0] = X + 7 * ( lower_right_[0] - X ) / 8;
+		}
 
-			if( upper_left_[1] < 0 )
-			{
-				lower_right_[1] -= upper_left_[1];
-				upper_left_[1]	 = 0;
+		if( lower_right_[1] - upper_left_[1] > 4 )
+		{
+			upper_left_[1]	= X - 7 * ( X - upper_left_[1] ) / 8;
+			lower_right_[1] = X + 7 * ( lower_right_[1] - X ) / 8;
+		}
 
-				if( lower_right_[1] >= cur_im_.height() )
-				{
-					lower_right_[1] = cur_im_.height() - 1;
-				}
-			}
+		if( lower_right_[2] - upper_left_[2] > 4 )
+		{
+			upper_left_[2]	= X - 7 * ( X - upper_left_[2] ) / 8;
+			lower_right_[2] = X + 7 * ( lower_right_[2] - X ) / 8;
+		}
+	}
 
-			if( upper_left_[2] < 0 )
-			{
-				lower_right_[2] -= upper_left_[2];
-				upper_left_[2]	 = 0;
+	if( view_action_ == ViewAction::zoom_out )
+	{
+		const int delta_x  = ( lower_right_[0] - upper_left_[0] ) / 8;
+		const int delta_y  = ( lower_right_[1] - upper_left_[1] ) / 8;
+		const int delta_z  = ( lower_right_[2] - upper_left_[2] ) / 8;
+		const int ndelta_x = delta_x ? delta_x : ( cur_im_.width()  > 1 ? 1 : 0 );
+		const int ndelta_y = delta_y ? delta_y : ( cur_im_.height() > 1 ? 1 : 0 );
+		const int ndelta_z = delta_z ? delta_z : ( cur_im_.depth()  > 1 ? 1 : 0 );
+		upper_left_[0]	-= ndelta_x;
+		upper_left_[1]	-= ndelta_y;
+		upper_left_[2]	-= ndelta_z;
+		lower_right_[0] += ndelta_x;
+		lower_right_[1] += ndelta_y;
+		lower_right_[2] += ndelta_z;
 
-				if( lower_right_[2] >= cur_im_.depth() )
-				{
-					lower_right_[2] = cur_im_.depth() - 1;
-				}
-			}
+		if( upper_left_[0] < 0 )
+		{
+			lower_right_[0] -= upper_left_[0];
+			upper_left_[0]	 = 0;
 
 			if( lower_right_[0] >= cur_im_.width() )
 			{
-				upper_left_[0] -= ( lower_right_[0] - cur_im_.width() + 1 );
 				lower_right_[0] = cur_im_.width() - 1;
-
-				if( upper_left_[0] < 0 )
-				{
-					upper_left_[0] = 0;
-				}
 			}
+		}
+
+		if( upper_left_[1] < 0 )
+		{
+			lower_right_[1] -= upper_left_[1];
+			upper_left_[1]	 = 0;
 
 			if( lower_right_[1] >= cur_im_.height() )
 			{
-				upper_left_[1] -= ( lower_right_[1] - cur_im_.height() + 1 );
 				lower_right_[1] = cur_im_.height() - 1;
-
-				if( upper_left_[1] < 0 )
-				{
-					upper_left_[1] = 0;
-				}
 			}
+		}
+
+		if( upper_left_[2] < 0 )
+		{
+			lower_right_[2] -= upper_left_[2];
+			upper_left_[2]	 = 0;
 
 			if( lower_right_[2] >= cur_im_.depth() )
 			{
-				upper_left_[2] -= ( lower_right_[2] - cur_im_.depth() + 1 );
 				lower_right_[2] = cur_im_.depth() - 1;
-
-				if( upper_left_[2] < 0 )
-				{
-					upper_left_[2] = 0;
-				}
 			}
 		}
 
-		if( view_action_ == ViewAction::go_right )
+		if( lower_right_[0] >= cur_im_.width() )
 		{
-			const int delta	 = ( lower_right_[0] - upper_left_[0] ) / 5;
-			const int ndelta = delta ? delta : ( cur_im_.width() > 1 ? 1 : 0 );
+			upper_left_[0] -= ( lower_right_[0] - cur_im_.width() + 1 );
+			lower_right_[0] = cur_im_.width() - 1;
 
-			if( lower_right_[0] + ndelta < cur_im_.width() )
+			if( upper_left_[0] < 0 )
 			{
-				upper_left_[0]	+= ndelta;
-				lower_right_[0] += ndelta;
-			}
-			else
-			{
-				upper_left_[0] += ( cur_im_.width() - lower_right_[0] - 1 );
-				lower_right_[0] = cur_im_.width() - 1;
+				upper_left_[0] = 0;
 			}
 		}
 
-		if( view_action_ == ViewAction::go_left )
+		if( lower_right_[1] >= cur_im_.height() )
 		{
-			const int delta	 = ( lower_right_[0] - upper_left_[0] ) / 5;
-			const int ndelta = delta ? delta : ( cur_im_.width() > 1 ? 1 : 0 );
+			upper_left_[1] -= ( lower_right_[1] - cur_im_.height() + 1 );
+			lower_right_[1] = cur_im_.height() - 1;
 
-			if( upper_left_[0] - ndelta >= 0 )
+			if( upper_left_[1] < 0 )
 			{
-				upper_left_[0]	-= ndelta;
-				lower_right_[0] -= ndelta;
-			}
-			else
-			{
-				lower_right_[0] -= upper_left_[0];
-				upper_left_[0]	 = 0;
+				upper_left_[1] = 0;
 			}
 		}
 
-		if( view_action_ == ViewAction::go_up )
+		if( lower_right_[2] >= cur_im_.depth() )
 		{
-			const int delta	 = ( lower_right_[1] - upper_left_[1] ) / 5;
-			const int ndelta = delta ? delta : ( cur_im_.height() > 1 ? 1 : 0 );
+			upper_left_[2] -= ( lower_right_[2] - cur_im_.depth() + 1 );
+			lower_right_[2] = cur_im_.depth() - 1;
 
-			if( upper_left_[1] - ndelta >= 0 )
+			if( upper_left_[2] < 0 )
 			{
-				upper_left_[1]	-= ndelta;
-				lower_right_[1] -= ndelta;
+				upper_left_[2] = 0;
 			}
-			else
-			{
-				lower_right_[1] -= upper_left_[1];
-				upper_left_[1]	 = 0;
-			}
-		}
-
-		if( view_action_ == ViewAction::go_down )
-		{
-			const int delta	 = ( lower_right_[1] - upper_left_[1] ) / 5;
-			const int ndelta = delta ? delta : ( cur_im_.height() > 1 ? 1 : 0 );
-
-			if( lower_right_[1] + ndelta < cur_im_.height() )
-			{
-				upper_left_[1]	+= ndelta;
-				lower_right_[1] += ndelta;
-			}
-			else
-			{
-				upper_left_[1] += ( cur_im_.height() - 1 - lower_right_[1] );
-				lower_right_[1] = cur_im_.height() - 1;
-			}
-		}
-
-		if( view_action_ == ViewAction::goto_next_frame )
-		{
-			cur_im_ = im_list_.previousImage();
-			disp.set_title( im_list_.getCurrentFileName().c_str() );
-		}
-
-		if( view_action_ == ViewAction::goto_previous_frame )
-		{
-			cur_im_ = im_list_.nextImage();
-			disp.set_title( im_list_.getCurrentFileName().c_str() );
 		}
 	}
-} // OMViewerApp::getSelection
+
+	if( view_action_ == ViewAction::go_right )
+	{
+		const int delta	 = ( lower_right_[0] - upper_left_[0] ) / 5;
+		const int ndelta = delta ? delta : ( cur_im_.width() > 1 ? 1 : 0 );
+
+		if( lower_right_[0] + ndelta < cur_im_.width() )
+		{
+			upper_left_[0]	+= ndelta;
+			lower_right_[0] += ndelta;
+		}
+		else
+		{
+			upper_left_[0] += ( cur_im_.width() - lower_right_[0] - 1 );
+			lower_right_[0] = cur_im_.width() - 1;
+		}
+	}
+
+	if( view_action_ == ViewAction::go_left )
+	{
+		const int delta	 = ( lower_right_[0] - upper_left_[0] ) / 5;
+		const int ndelta = delta ? delta : ( cur_im_.width() > 1 ? 1 : 0 );
+
+		if( upper_left_[0] - ndelta >= 0 )
+		{
+			upper_left_[0]	-= ndelta;
+			lower_right_[0] -= ndelta;
+		}
+		else
+		{
+			lower_right_[0] -= upper_left_[0];
+			upper_left_[0]	 = 0;
+		}
+	}
+
+	if( view_action_ == ViewAction::go_up )
+	{
+		const int delta	 = ( lower_right_[1] - upper_left_[1] ) / 5;
+		const int ndelta = delta ? delta : ( cur_im_.height() > 1 ? 1 : 0 );
+
+		if( upper_left_[1] - ndelta >= 0 )
+		{
+			upper_left_[1]	-= ndelta;
+			lower_right_[1] -= ndelta;
+		}
+		else
+		{
+			lower_right_[1] -= upper_left_[1];
+			upper_left_[1]	 = 0;
+		}
+	}
+
+	if( view_action_ == ViewAction::go_down )
+	{
+		const int delta	 = ( lower_right_[1] - upper_left_[1] ) / 5;
+		const int ndelta = delta ? delta : ( cur_im_.height() > 1 ? 1 : 0 );
+
+		if( lower_right_[1] + ndelta < cur_im_.height() )
+		{
+			upper_left_[1]	+= ndelta;
+			lower_right_[1] += ndelta;
+		}
+		else
+		{
+			upper_left_[1] += ( cur_im_.height() - 1 - lower_right_[1] );
+			lower_right_[1] = cur_im_.height() - 1;
+		}
+	}
+
+	if( view_action_ == ViewAction::goto_next_frame )
+	{
+		cur_im_ = im_list_.previousImage();
+		disp.set_title( im_list_.getCurrentFileName().c_str() );
+	}
+
+	if( view_action_ == ViewAction::goto_previous_frame )
+	{
+		cur_im_ = im_list_.nextImage();
+		disp.set_title( im_list_.getCurrentFileName().c_str() );
+	}
+	if( view_action_ == ViewAction::reset_view )
+	{
+		resetView(disp);
+	}
+	view_action_ = ViewAction::no_action;
+}
 
 /*
  *	Modified from CImg<T>._display() since it already has most of the logic
@@ -365,8 +384,7 @@ int OMViewerApp::getSelection( const cimg_library::CImg<float>& subim, cimg_libr
  */
 int OMViewerApp::display( CImgDisplay& disp )
 {
-	bool normalize = true;
-	int	 key;
+	int key;
 
 	old_w_			= old_h_ = key = 0;
 	upper_left_[0]	= upper_left_[1] = upper_left_[2] = 0;
@@ -376,20 +394,20 @@ int OMViewerApp::display( CImgDisplay& disp )
 	XYZ_[0]			= ( lower_right_[0] - upper_left_[0] ) / 2;
 	XYZ_[1]			= ( lower_right_[1] - upper_left_[1] ) / 2;
 	XYZ_[2]			= ( lower_right_[2] - upper_left_[2] ) / 2;
+	resetView(disp);
 	disp.show().flush();
 
 	CImg<float> zoom;
 
-	while( !disp.is_closed() )
+	while( !disp.is_closed() && disp.key() != cimg::keyQ)
 	{
-		if( reset_view_ )
-		{
-			resetView( disp );
-			reset_view_ = false;
-		}
-
-		if( upper_left_[0] == 0 && upper_left_[1] == 0 && upper_left_[2] == 0 && lower_right_[0] == cur_im_.width() -
-			1 && lower_right_[1] == cur_im_.height() - 1 && lower_right_[2] == cur_im_.depth() - 1 )
+		cur_view_im_ = cur_im_;
+		if( upper_left_[0]  == 0 &&
+			upper_left_[1]  == 0 &&
+			upper_left_[2]  == 0 &&
+			lower_right_[0] == cur_im_.width() - 1 &&
+			lower_right_[1] == cur_im_.height() - 1 &&
+			lower_right_[2] == cur_im_.depth() - 1 )
 		{
 			if( cur_im_.is_empty() )
 			{
@@ -402,7 +420,7 @@ int OMViewerApp::display( CImgDisplay& disp )
 		}
 		else
 		{
-			zoom = cur_im_.get_crop( upper_left_[0],
+			zoom = cur_view_im_.get_crop( upper_left_[0],
 									 upper_left_[1],
 									 upper_left_[2],
 									 lower_right_[0],
@@ -418,14 +436,29 @@ int OMViewerApp::display( CImgDisplay& disp )
 
 		if( !cur_im_.is_empty() && !disp.is_fullscreen() && resize_disp_ )
 		{
-			resizeView( disp, tw, th);
+			resizeView( disp, tw, th );
 			resize_disp_ = false;
 		}
 		old_w_ = tw;
 		old_h_ = th;
 
+		if(equalize_depth_image_)
+		{
+			float new_val = cur_view_im_.max() + 10;
+			cimg_forXY(cur_view_im_, x, y)
+			{
+				if (cur_view_im_(x,y) == 0) {
+					cur_view_im_(x,y) = new_val;
+				}
+			}
+		}
+		if(histogram_equalize_)
+		{
+			const int numBins = 100;
+			cur_view_im_ = cur_view_im_.equalize(numBins);
+		}
 
-		const CImg<float>& visu = zoom ? zoom : cur_im_;
+		const CImg<float>& visu = zoom ? zoom : cur_view_im_ ;
 
 		if( cur_im_.width() > 1  && visu.width() == 1 )
 		{
@@ -443,6 +476,7 @@ int OMViewerApp::display( CImgDisplay& disp )
 		}
 
 		key = getSelection( visu, disp );
+		performViewAction( disp );
 		disp.wait( 100 );
 	}
 	disp.set_key( key );
--- src/OMViewerApp.hpp
+++ src/OMViewerApp.hpp
@@ -1,39 +1,52 @@
 #include "ImageList.hpp"
 
-namespace omviewer {
+namespace omviewer
+{
+
 enum class ViewAction
 {
-	zoom_in, zoom_out, go_left, go_right, go_down, go_up, goto_next_frame, goto_previous_frame, go_in_center
+	no_action,
+	reset_view,
+	zoom_in,
+	zoom_out,
+	go_left,
+	go_right,
+	go_down,
+	go_up,
+	goto_next_frame,
+	goto_previous_frame,
+	go_in_center
 };
 
-class OMViewerApp {
+class OMViewerApp
+{
 public:
-
-	OMViewerApp( const std::string& image_dir ):im_list_( image_dir ) {}
-
+	OMViewerApp(const std::string& image_dir) : im_list_(image_dir) {};
 	void run();
-
 private:
-
-	int	 display( cimg_library::CImgDisplay& disp );
-	void resetView( cimg_library::CImgDisplay& disp );
-	void resizeView( cimg_library::CImgDisplay& disp,
-					 unsigned int				tw,
-					 unsigned int				th );
-	int	 getSelection( const cimg_library::CImg<float>& im,
-					   cimg_library::CImgDisplay	  & disp );
+	int display( cimg_library::CImgDisplay & disp);
+	void resetView(cimg_library::CImgDisplay & disp);
+	void resizeView(cimg_library::CImgDisplay & disp, unsigned int tw, unsigned int th);
+	int getSelection(const cimg_library::CImg<float>& im, cimg_library::CImgDisplay & disp);
+	void performViewAction(cimg_library::CImgDisplay & disp);
 
 	ImageList im_list_;
+	ViewAction view_action_;
 	cimg_library::CImg<float> cur_im_;
+	cimg_library::CImg<float> cur_view_im_;
 
 	unsigned int XYZ_[3];
 	int upper_left_[3];
 	int lower_right_[3];
 	int old_w_;
 	int old_h_;
-	ViewAction view_action_;
-	bool reset_view_;
+
 	bool center_view_;
 	bool resize_disp_;
+
+	// per frame actions
+	bool equalize_depth_image_;
+	bool histogram_equalize_;
+
 };
 } // namespace
