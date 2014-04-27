6220c460cb509e3162b32b907312dc87bc1f9ab6
Miroslav Kobetski <miroslav.kobetski@gmail.com>
2014-04-28 01:31:48 +0200

    Can start at specific file

--- src/ImageList.cpp
+++ src/ImageList.cpp
@@ -3,74 +3,96 @@
 
 #include "ImageList.hpp"
 
-
 using namespace cimg_library;
 
-namespace omviewer
+namespace omviewer {
+ImageList::ImageList( const std::string& dir_name ):cur_i_( 0 ), dir_name_( dir_name )
 {
-	ImageList::ImageList(const std::string& dir_name) : cur_i_(0)
+	readFiles( dir_name_ );
+}
+
+CImg<float>ImageList::nextImage()
+{
+	cur_i_++;
+
+	if( cur_i_ >= file_names_.size() )
 	{
-		readFiles(dir_name);
+		cur_i_ = 0;
 	}
-	
-	CImg<float> ImageList::nextImage()
+	return getImage( cur_i_ );
+}
+
+CImg<float>ImageList::previousImage()
+{
+	cur_i_--;
+
+	if( cur_i_ < 0 )
 	{
-		cur_i_++;
-		if(cur_i_ >= file_names_.size())
-		{
-			cur_i_ = 0;
-		}
-		return getImage(cur_i_);
+		cur_i_ = file_names_.size() - 1;
+	}
+	return getImage( cur_i_ );
+}
+
+CImg<float>ImageList::getImage( size_t i ) const
+{
+	CImg<float> im( ( dir_name_ + file_names_.at( i ) ).c_str() );
+	return im;
+}
+
+std::string ImageList::getCurrentFileName() const
+{
+	return file_names_.at( cur_i_ );
+}
+
+/*
+ *	Private helper functions
+ */
+void ImageList::readFiles( const std::string& dir_name )
+{
+	// List all files in directory (default is ./)
+	namespace fs =  boost::filesystem;
+	fs::path image_directory( dir_name );
+	fs::directory_iterator end_iter;
+	std::string starting_file( "" );
+
+	// actual file rather than dir
+	if( !fs::is_directory( image_directory ) )
+	{
+		starting_file = image_directory.leaf().string();
+		image_directory = image_directory.parent_path();
 	}
 
-	CImg<float> ImageList::previousImage()
+	if( fs::exists( image_directory ) )
 	{
-		cur_i_--;
-		if(cur_i_ < 0)
+		for( fs::directory_iterator dir_iter( image_directory ); dir_iter != end_iter; ++dir_iter )
 		{
-			cur_i_ = file_names_.size() - 1;
-		}
-		return getImage(cur_i_);
-	}
-
-	CImg<float> ImageList::getImage(size_t i) const
-	{
-		CImg<float> im(file_names_.at(i).c_str());
-		return im;
-	}
-
-	std::string ImageList::getCurrentFileName() const
-	{
-		return file_names_.at(cur_i_);
-	}
-	/*
-	 *	Private helper functions
-	 */
-	void ImageList::readFiles(const std::string & dir_name)
-	{
-		// List all files in directory (default is ./)
-		namespace fs =  boost::filesystem;
-		fs::path image_directory(dir_name);
-		fs::directory_iterator end_iter;
-		if(fs::exists( image_directory ) && fs::is_directory( image_directory ))
-		{
-			for(fs::directory_iterator dir_iter( image_directory ); dir_iter != end_iter; ++dir_iter)
+			if( fs::is_regular_file( dir_iter->status() ) )
 			{
-				if(fs::is_regular_file( dir_iter->status() ))
+				for( auto& ext : supported_image_types_ )
 				{
-					for(auto & ext : supported_image_types_)
+					if( dir_iter->path().extension() == ext )
 					{
-						if(dir_iter->path().extension() == ext)
-						{
-							file_names_.push_back(dir_iter->path().string());
-						}
+						file_names_.push_back( dir_iter->path().filename().string() );
 					}
-
 				}
 			}
 		}
-
-		// Sort files since os doesn't guarantee ls-order
-		std::sort(file_names_.begin(), file_names_.end());
 	}
+
+	dir_name_ = image_directory.string();
+
+	if( dir_name_.back() != '/' )
+	{
+		dir_name_.push_back( '/' );
+	}
+
+	// Sort files since os doesn't guarantee ls-order
+	std::sort( file_names_.begin(), file_names_.end() );
+
+	// find index of starting file
+	if( starting_file != "" )
+	{
+		cur_i_ = std::find( file_names_.begin(), file_names_.end(), starting_file ) - file_names_.begin();
+	}
+} // ImageList::readFiles
 } // namespace
--- src/ImageList.hpp
+++ src/ImageList.hpp
@@ -5,25 +5,24 @@
 
 #include <CImg.h>
 
-namespace omviewer
-{
-
-class ImageList
-{
+namespace omviewer {
+class ImageList {
 public:
-	ImageList(const std::string& dir_name = "./");
-	cimg_library::CImg<float> nextImage();
-	cimg_library::CImg<float> previousImage();
-	cimg_library::CImg<float> getImage(size_t i) const;
-	std::string getCurrentFileName() const;
+
+	ImageList( const std::string& dir_name );
+	cimg_library::CImg<float>nextImage();
+	cimg_library::CImg<float>previousImage();
+	cimg_library::CImg<float>getImage( size_t i ) const;
+	std::string				 getCurrentFileName() const;
 
 private:
+
 	int cur_i_;
 	std::vector<std::string> file_names_;
+	std::string dir_name_;
 
-	const std::vector<std::string> supported_image_types_ = {".png", ".jpg", ".ppm", ".jpeg", ".gif", ".tiff", ".bmp"}; // c++11 required
+	const std::vector<std::string> supported_image_types_ = { ".png", ".jpg", ".ppm", ".jpeg", ".gif", ".tiff", ".bmp" };
 
-	void readFiles(const std::string & dir_name);
+	void readFiles( const std::string& dir_name );
 };
-
 } // namespace
